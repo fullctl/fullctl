224e9c88c575d928db8610cd4bc1b512cd49e2e3
Stefan Pratter <stefan@20c.com>
2021-02-03 12:20:08 +0000

    add CachedObjectMixin and OrgQuerysetMixin

---
+++ src/fullctl/django/rest/mixins.py
@@ -0,0 +1,23 @@
+class CachedObjectMixin:
+
+    """
+    Assures that a views get_object() call only
+    queries the db once
+    """
+
+    def get_object(self):
+        if getattr(self, "_obj", None) is None:
+            self._obj = super().get_object()
+
+        return self._obj
+
+
+class OrgQuerysetMixin:
+    """
+    For objects with URLs that require an "org_tag", this filters
+    the resulting queryset by matching the instance org to the
+    provided slug.
+    """
+    def get_queryset(self):
+        org_tag = self.kwargs["org_tag"]
+        return self.queryset.filter(instance__org__slug=org_tag)
