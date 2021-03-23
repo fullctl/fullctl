0566654cdca66e025e50e070aeef15459af27fa9
Stefan Pratter <stefan@20c.com>
2021-03-23 15:53:33 +0000

    peeringdb asn autocomplete

--- src/fullctl/django/autocomplete/views.py
+++ src/fullctl/django/autocomplete/views.py
@@ -15,6 +15,29 @@ class peeringdb_ix(autocomplete.Select2QuerySetView):
         return ixlan.ix.name
 
 
+class peeringdb_asn(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = pdb_models.Network.objects.filter(status="ok")
+        if self.q:
+            try:
+                int(self.q)
+                qs = qs.filter(asn__istartswith=self.q)
+            except ValueError:
+                qs = qs.filter(name__istartswith=self.q)
+
+        if not qs.count():
+            return [pdb_models.Network(asn=self.q, name="Unknown ASN")]
+
+        return qs
+
+    def get_result_label(self, item):
+        return "AS{} {}".format(item.asn, item.name)
+
+    def get_result_value(self, item):
+        return item.asn
+
+
+
 class peeringdb_org(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         qs = pdb_models.Organization.objects.filter(status="ok")
