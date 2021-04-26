231d3f7c2d8cd1676e286fd212b46ea6b71bc5d3
Stefan Pratter <stefan@20c.com>
2021-04-26 12:14:46 +0000

    better remote permissions error handling

--- src/fullctl/django/auth.py
+++ src/fullctl/django/auth.py
@@ -1,8 +1,21 @@
+import json
 import django_grainy.remote
 import django_grainy.util
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+
+class RemotePermissionsError(IOError):
+    def __init__(self):
+
+        if not getattr(settings, "SERVICE_KEY", None):
+            msg = "No SERVICE_KEY specified"
+        else:
+            msg = (
+                "Could not parse response from aaactl for remote permissions."
+                "Please check aaactl logs for diagnostic details"
+            )
+        super().__init__(msg)
 
 class Permissions(django_grainy.util.Permissions):
     pass
@@ -18,6 +31,12 @@ class RemotePermissions(django_grainy.remote.Permissions):
 
     def __init__(self, obj):
         super().__init__(obj, **settings.GRAINY_REMOTE)
+
+    def fetch(self, *args, **kwargs):
+        try:
+            return super().fetch(*args, **kwargs)
+        except json.decoder.JSONDecodeError as exc:
+            raise RemotePermissionsError()
 
     def prepare_request(self, params, headers):
         try:
@@ -40,6 +59,5 @@ def permissions(permission_holder, refresh=False):
         perms = Permissions(permission_holder)
     else:
         perms = RemotePermissions(permission_holder)
-        print("PERMS", perms.pset.permissions)
     permission_holder._fullctl_permissions = perms
     return perms
