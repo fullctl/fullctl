babf7d674b0a09a72ade7377bb42ca8cb8ad52bf
Stefan Pratter <stefan@20c.com>
2021-05-18 11:25:27 +0000

    error pages (#15)

--- src/fullctl/django/views.py
+++ src/fullctl/django/views.py
@@ -1,9 +1,14 @@
+import sys
 import re
+import datetime
 
 # from django.conf import settings
 from django.http import Http404, HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.utils.safestring import mark_safe
+
+
+from django.template import loader
 
 from fullctl.django.decorators import require_auth
 
@@ -26,3 +31,46 @@ def diag(request):
         txt += f"{k}: {v}\n"
 
     return HttpResponse(mark_safe(f"<div><pre>Meta:\n{txt}</pre></div>"))
+
+
+def handle_error(request, exception, status):
+    if exception:
+        request.error_response = exception
+        exc_type = exception.__class__
+    else:
+        exc_type, exception, tb = sys.exc_info()
+
+    request.error_response = exception
+
+    context = {
+        "error": {
+            "status" : status,
+            "type_description" : f"{exc_type}",
+            "description" : f"{exception}",
+            "path": request.path,
+            "ip_address" : request.META.get("HTTP_X_FORWARDED_FOR"),
+            "referer" : request.META.get("HTTP_REFERER"),
+            "timestamp": datetime.datetime.utcnow()
+        }
+    }
+
+    response = render(request, f"common/errors/{status}.html", context)
+    response.status_code = status
+    return response
+
+def handle_error_500(request, exception=None):
+    return handle_error(request, exception, 500)
+
+def handle_error_404(request, exception=None):
+    return handle_error(request, exception, 404)
+
+def handle_error_401(request, exception=None):
+    return handle_error(request, exception, 401)
+
+def handle_error_400(request, exception=None):
+    return handle_error(request, exception, 400)
+
+def handle_error_403(request, exception=None):
+    return handle_error(request, exception, 403)
+
+
