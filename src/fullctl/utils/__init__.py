7a3dd431799323f5ebd5a1eb84063ad4f4c556e6
Stefan Pratter <stefan@20c.com>
2022-08-02 09:27:20 +0000

    add utils

---
+++ src/fullctl/utils/__init__.py
@@ -0,0 +1,6 @@
+import functools
+
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
