ae122788b1a90e268603d8978070f2b101da8da6
Stefan Pratter <stefan@20c.com>
2023-02-02 19:24:11 +0200

    Fix pdb sync (#98)
    * fix fullctl_peeringdb_sync getting ratelimited when fetching deleted relations
    
    * linting

--- src/fullctl/django/management/commands/fullctl_peeringdb_sync.py
+++ src/fullctl/django/management/commands/fullctl_peeringdb_sync.py
@@ -1,10 +1,30 @@
+import time
+
 from django.conf import settings
-from peeringdb import initialize_backend, resource
+from peeringdb import _fetch, initialize_backend, resource
 from peeringdb.client import Client
 
 from fullctl.django.management.commands.base import CommandInterface
 
 PDB_API_URL = getattr(settings, "PDB_API_URL", "https://www.peeringdb.com/api")
+
+
+# Temporary fix to handle fetching of deleted relations triggering
+# rate-limiting from the peeringdb API
+#
+# Ideally this will be fixed in the peeringdb-py client (#76), but for now
+# this should do the trick.
+
+
+def fetch_deleted(self, R, pk, depth):
+    def req():
+        time.sleep(2)
+        return self.all(R.tag, id=pk, since=1, depth=depth)
+
+    return self._req(req)
+
+
+_fetch.Fetcher.fetch_deleted = fetch_deleted
 
 
 class Command(CommandInterface):
