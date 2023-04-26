fdb2dd3fc3712402715ae4a4c285550a8ed0505f
Stefan Pratter <stefan@20c.com>
2023-04-27 00:24:59 +0300

    ux fixes for input group select elements (#145)
    * ux fixes for input group select elements
    
    * datetime formatter
    nowrap css rule

--- src/fullctl/django/static/common/v2/app.js
+++ src/fullctl/django/static/common/v2/app.js
@@ -65,6 +65,22 @@ fullctl.formatters.meta_data = (value) => {
 }
 
 /**
+ * Formats a datetime string into a human readable format
+ */
+fullctl.formatters.datetime = (value) => {
+  const date = new Date(value);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+/**
  * Formats `True` and `False` as checkmark and cross
  * @method pretty_bool
  * @param {String} value value of cell
