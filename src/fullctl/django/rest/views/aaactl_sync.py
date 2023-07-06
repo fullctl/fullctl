edad42de954630903139d848b3ddcd77e3d802a9
Stefan Pratter <stefan@20c.com>
2023-07-06 22:38:31 +0300

    sync new user creation through service bridge (#171)
    * sync new user creation through service bridge
    
    * linting

--- src/fullctl/django/rest/views/aaactl_sync.py
+++ src/fullctl/django/rest/views/aaactl_sync.py
@@ -8,6 +8,7 @@ only exists on the internal api key used for service bridging
 
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 import fullctl.django.models as models
@@ -39,11 +40,23 @@ class User(CachedObjectMixin, viewsets.GenericViewSet):
     lookup_field = "social_auth__uid"
     lookup_url_kwarg = "remote_id"
 
+    @action(detail=False, methods=["put"], url_path="(?P<aaactl_id>[^/.]+)")
     @grainy_endpoint("aaactl_sync.user")
-    def update(self, request, remote_id, *args, **kwargs):
-        user = self.get_object()
-        serializer = self.serializer_class(instance=user, data=request.data)
+    def create_or_update(self, request, aaactl_id, *args, **kwargs):
+        """
+        Creates / Updates user by their aaactl id (social_auth__uid)
+        """
+
+        try:
+            user = get_user_model().objects.get(social_auth__uid=aaactl_id)
+        except get_user_model().DoesNotExist:
+            user = None
+
+        serializer = self.serializer_class(
+            instance=user, data=request.data, context={"remote_id": aaactl_id}
+        )
         if not serializer.is_valid():
+            print(serializer.errors)
             return BadRequest(serializer.errors)
         serializer.save()
         return Response(serializer.data)
