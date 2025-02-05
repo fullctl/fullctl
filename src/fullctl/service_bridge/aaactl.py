4f04c4932cc8177bfa25d923b5cf77eb276f33fd
Sola Babatunde <solababatunde12@gmail.com>
2025-02-05 09:44:55 +0000

    remove return_as_string param

--- src/fullctl/service_bridge/aaactl.py
+++ src/fullctl/service_bridge/aaactl.py
@@ -276,7 +276,6 @@ class PointOfContact(Aaactl):
         entity: int | None = None,
         delivery_type: str = "email",
         poc_type: str = "notifications",
-        return_as_string: bool = False,
     ) -> list[str] | str:
         """
         Get the service alert recipients for an org regardless
@@ -308,15 +307,8 @@ class PointOfContact(Aaactl):
             if entity and config.get("entity") != entity:
                 continue
 
-            recipients = config.get("recipients", [])
-
-            # Why do we need this here?
-            if return_as_string:
-                return ", ".join(recipients)
-
-            return recipients
-
-        return "" if return_as_string else []
+            return config.get("recipients", [])
+        return []
 
     def get_email_alert_recipients(
         self, org, service: str, entity: int | None = None
@@ -330,6 +322,4 @@ class PointOfContact(Aaactl):
             entity=entity,
             delivery_type="email",
             poc_type="notifications",
-            # why?
-            return_as_string=True,
         )
